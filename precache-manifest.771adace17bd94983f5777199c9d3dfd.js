60bffaed25794bc34096f048fec7c9279386c35b
Divyesh Prajapati <divyeshprajapati100@gmail.com>
2020-06-20 00:05:38 +0530

    Updates

---
+++ precache-manifest.771adace17bd94983f5777199c9d3dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc948159405ee55ead521f7d0fd598c",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "5ea7cdb9024c4ea62dd4",
+    "url": "/weather_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ff889c640761f83d56f",
+    "url": "/weather_app/static/js/2.b4542ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.b4542ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea7cdb9024c4ea62dd4",
+    "url": "/weather_app/static/js/main.58e3ee92.chunk.js"
+  },
+  {
+    "revision": "c47e7f26296995c9e615",
+    "url": "/weather_app/static/js/runtime-main.ba01df11.js"
+  }
+]);
